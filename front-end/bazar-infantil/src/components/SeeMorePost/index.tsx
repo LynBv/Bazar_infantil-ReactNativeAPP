2d01c7a836c148b62e05833ce7a630d09e18cc22
DaviSoaresA <didequinho@gmail.com>
2024-11-21 19:21:05 -0300

    Consertado as minhas besteiras

--- front-end/bazar-infantil/src/components/SeeMorePost/index.tsx
+++ front-end/bazar-infantil/src/components/SeeMorePost/index.tsx
@@ -1,20 +1,23 @@
 import { Image, ScrollView, Text, View } from "react-native";
 import { styles } from "./style";
 import React from "react";
-import { ArrayPostagem } from "../FeedRow/type";
+import { ArrayPostagem, PropsPostagem } from "../FeedRow/type";
 import { PropsSeeMore } from "./type";
 import { ButtonTypes } from "../ButtonTypes";
 import { useNavigation } from "@react-navigation/native";
 
-export const SeeMorePost = ({ postagem }: { postagem: PropsSeeMore }) => {
+export const SeeMorePost = ({ postagem }: { postagem: PropsPostagem }) => {
+  const avatarImage =
+    "data:image/png;base64," + postagem.postagem.usuarioDTO.base64;
+  const postImage = "data:image/png;base64," + postagem.postagem.foto[0].dados;
 
   const navigation = useNavigation();
 
-  function addCarrinho(){
+  function addCarrinho() {
     //add Para o carrinho
   }
 
-  function comprar(){
+  function comprar() {
     //compra direta
   }
 
@@ -22,24 +25,30 @@ export const SeeMorePost = ({ postagem }: { postagem: PropsSeeMore }) => {
     <>
       <ScrollView style={styles.container}>
         <View style={styles.ownerInfo}>
-          <Image
-            source={postagem.postagem.fotoUsuario}
-            style={styles.ownerAvatar}
-          />
-          <Text style={styles.ownerName}>{postagem.postagem.nomeUsuario}</Text>
+          <Image source={{ uri: avatarImage }} style={styles.ownerAvatar} />
+          <Text style={styles.ownerName}>
+            {postagem.postagem.usuarioDTO.nome}
+          </Text>
         </View>
         <View style={styles.postInfo}>
-          <Image
-            source={postagem.postagem.fotoPostagem}
-            style={styles.postPhoto}
-          />
-          <Text style={styles.postDescricao}>{postagem.postagem.conteudo}</Text>
-          <Text style={styles.postData}>{postagem.postagem.dataPostagem}</Text>
+          <Text style={styles.postTitle}>{postagem.postagem.titulo}</Text>
+          <Image source={{ uri: postImage }} style={styles.postPhoto} />
+          <Text style={styles.postDescricao}>
+            {postagem.postagem.descricao}
+          </Text>
+          <View style={styles.postSmallDetails}>
+            <Text style={styles.postCateg}>{postagem.postagem.categoriasGenero}</Text>
+            <Text style={styles.postCateg}>{postagem.postagem.categoriasIdade}</Text>
+            <Text style={styles.postData}>{postagem.postagem.dataCriacao}</Text>
+          </View>
           <Text style={styles.postPreco}>R$ {postagem.postagem.preco}</Text>
         </View>
         <View>
-          <ButtonTypes title="Adicionar ao Carrinho" handleFunction={addCarrinho}/>
-          <ButtonTypes title="Comprar" handleFunction={comprar}/>
+          <ButtonTypes
+            title="Adicionar ao Carrinho"
+            handleFunction={addCarrinho}
+          />
+          <ButtonTypes title="Comprar" handleFunction={comprar} />
         </View>
       </ScrollView>
       <View style={styles.shadow}></View>
